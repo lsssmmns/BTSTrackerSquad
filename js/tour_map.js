68159778e03834e67224f9405837509b3fd0f589
lsssmmns <lsssmmns@gmail.com>
2022-04-25 22:53:12 -0700

    add photos

--- js/tour_map.js
+++ js/tour_map.js
@@ -8,28 +8,28 @@ let zl = 2;
 let path_RB = "https://raw.githubusercontent.com/lsssmmns/BTSTrackerSquad/main/data/RedBulletTour.xlsx.csv";
 let markers_RB = L.featureGroup();
 let tour_RB;
-let im_RB = "\photos\rb.jpg";
+let im_RB = "https://raw.githubusercontent.com/lsssmmns/BTSTrackerSquad/main/photos/rb.jpg";
 // wake up tour data
 let path_WU = "https://raw.githubusercontent.com/lsssmmns/BTSTrackerSquad/main/data/WakeUp.xlsx.csv";
 let markers_WU = L.featureGroup();
 let tour_WU;
-let im_WU = "\photos\wu.jpg";
+let im_WU = "https://raw.githubusercontent.com/lsssmmns/BTSTrackerSquad/main/photos/wu.jpg";
 // map of the soul tour data
 let path_MotS = "https://raw.githubusercontent.com/lsssmmns/BTSTrackerSquad/main/data/MapofTheSoul.xlsx.csv";
 let markers_MotS = L.featureGroup();
 let tour_MotS;
-let im_MotS = "\photos\MotS.jpg";
+let im_MotS = "https://raw.githubusercontent.com/lsssmmns/BTSTrackerSquad/main/photos/MotS.jpg";
 
 
 // initialize
 $( document ).ready(function() {
 	createMap(lat,lon,zl);
     // red
-	tour_RB = readCSV(path_RB, markers_RB, '#cd4d5e', tour_RB, "The Red Bullet");
+	tour_RB = readCSV(path_RB, markers_RB, '#cd4d5e', tour_RB, "The Red Bullet", im_RB);
     // yellow
-    tour_WU = readCSV(path_WU,markers_WU, '#e19a0c', tour_WU, "Wake Up: Open Your Eyes");
+    tour_WU = readCSV(path_WU,markers_WU, '#e19a0c', tour_WU, "Wake Up: Open Your Eyes", im_WU);
     // blue
-    tour_MotS = readCSV(path_MotS,markers_MotS, '#456a96', tour_MotS,"Map of the Soul");
+    tour_MotS = readCSV(path_MotS,markers_MotS, '#456a96', tour_MotS,"Map of the Soul", im_MotS);
     let layers = {
         "The Red Bullet 2014" : markers_RB,
         "Wake Up: Open Your Eyes 2015" : markers_WU,
@@ -86,7 +86,7 @@ function mapCSV(markers, tour_color, data, tour_name, pic){
             });
 		}else{
             show = show.on('mouseover',function(){
-			    this.bindPopup(`<div id="pop"><p id="event" style="color:${tour_color};">${tour_name}</p><p><b>BTS performed at the ${item.venue} in ${item.city} on ${item.start}</b></p></div>`).openPopup()
+			    this.bindPopup(`<div id="pop"><p id="event" style="color:${tour_color};">${tour_name}</p><img id=im src="${pic}"><p><b>BTS performed at the ${item.venue} in ${item.city} on ${item.start}</b></p></div>`).openPopup()
             });
         };
 
